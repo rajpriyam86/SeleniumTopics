d3221937a7ab9fcb3481d4f07dd34aeeb02e2768
Priyam Raj <rajpriyam86@gmail.com>
2025-03-01 08:29:26 +0530

    2nd commit

--- src/CoreJava/ReverseArrayList.java
+++ src/CoreJava/ReverseArrayList.java
@@ -1,5 +1,11 @@
 package CoreJava;
 
+import java.util.ArrayList;
+
 public class ReverseArrayList {
+	public static void main(String[] args) {
+		ArrayList<Integer> arr = new ArrayList<>();
+		arr.add(10);
+	}
 
 }
